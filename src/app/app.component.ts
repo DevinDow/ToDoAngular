57cc6fbaee6d61d83c5b8a5589c5bb391e518f2e
Devin Dow <DevinDow@gmail.com>
2018-10-18 08:31:18 -0700

    log in and fetch lists

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { login } from '../assets/javascript/apis.js'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ToDoAngular';
+
+  ngOnInit() {
+    console.log("*** AppComponent.ngOnInit()")
+
+    login('DevinDow@gmail.com', 'password')
+
+  }
+
 }
